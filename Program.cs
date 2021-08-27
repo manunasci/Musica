67eae09f84b40a44530b2e0608078b0dcc3fe697
UlissesSilva08 <ulissesmasterchief@gmail.com>
2021-08-27 20:24:27 -0300

    Programa que exibe a música

---
+++ Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+
+namespace Musica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor=ConsoleColor.Red;
+            Console.WriteLine("Tienpo Pa Olvidar (part.Becky G)");
+
+            Console.ForegroundColor=ConsoleColor.DarkYellow;
+            Console.WriteLine("Abraham Mateo");
+
+            Console.WriteLine();
+
+            Console.ForegroundColor=ConsoleColor.Blue;
+
+            Console.WriteLine("Me hiciste infeliz");
+            Console.WriteLine("Soy un hombre que perdona lo que sea,");
+            Console.WriteLine("menos un desliz");
+            Console.WriteLine("Igual París sin la torre Eiffel seguirá siendo");
+            Console.WriteLine("París");
+            Console.WriteLine("Aquí no vuelvas, please");
+            Console.WriteLine("");
+            Console.WriteLine("No te necesito");
+            Console.WriteLine("Lo que necesito es");  
+
+            Console.WriteLine();
+
+            Console.ForegroundColor=ConsoleColor.DarkMagenta;
+
+            Console.WriteLine("Tiempo pa' olvidar");
+            Console.WriteLine("Copas pa' beber");
+            Console.WriteLine("Y algo pa' este mal");
+            Console.WriteLine("Porque me voy a enloquecer");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Tiempo pa' olvidar");
+            Console.WriteLine("Copas pa' beber");
+            Console.WriteLine("Y algo pa' este mal");
+            Console.WriteLine("Porque me voy a enloquecer");
+
+            Console.WriteLine();
+
+            Console.ForegroundColor=ConsoleColor.Blue;
+
+            Console.WriteLine("(Becky, Becky, Becky G)");
+
+            Console.WriteLine("Dame tiempo, de esto salgo");
+            Console.WriteLine("Esto solo es un tago amargo");
+            Console.WriteLine("Duele, pero puedo soportarlo");
+            Console.WriteLine("No te olvido, pero voy a superarlo");
+
+            Console.WriteLine();
+
+            Console.WriteLine("De eso, yo estoy segura");
+            Console.WriteLine("El tiempo todo lo cura");
+            Console.WriteLine("Cualquiera se desgasta");
+            Console.WriteLine("Con tu distancia tengo, con eso basta");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Primero yo, segundo yo, tercero yo");
+            Console.WriteLine("La prioridad de ahora en adelante: Yo");
+            Console.WriteLine("Te tenía en un pedestal");
+            Console.WriteLine("Ahora solo quiero");
+
+            Console.WriteLine();
+
+            Console.ForegroundColor=ConsoleColor.DarkMagenta;
+
+            Console.WriteLine("Tiempo pa' olvidar");
+            Console.WriteLine("Copas pa' beber");
+            Console.WriteLine("Y algo pa' este mal");
+            Console.WriteLine("Porque me voy a enloquecer (enlouqecer");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Tiempo pa' olvidar");
+            Console.WriteLine("Copas pa' beber");
+            Console.WriteLine("Y algo pa' este mal");
+            Console.WriteLine("Porque me voy a enloquecer");
+
+            Console.WriteLine();
+
+            Console.ForegroundColor=ConsoleColor.Blue;
+
+            Console.WriteLine("Se te olvidó recoger todos los besos que");
+            Console.WriteLine("dejaste aquí");
+            Console.WriteLine("Decía la gente realmente que no eras pa' mí");
+            Console.WriteLine("Y no lo vi, yo solito solo me jodí");
+            Console.WriteLine("Solo fuiste tiempo de reloj que perdí");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Pero me acuesto y recuerdo tu cuerpo en la");
+            Console.WriteLine("cama");
+            Console.WriteLine("De nuevo, el insomnio me llama");
+            Console.WriteLine("Cásntas veces me hizo un drama");
+            Console.WriteLine("No vale la pena llarar por una persona que no");
+            Console.WriteLine("ama");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Me hiciste infeliz");
+            Console.WriteLine("Eres un hombre que perdona lo que sea,");
+            Console.WriteLine("menos un desliz");
+            Console.WriteLine("Ingual París sin la torre Eiffel seguirá siendo");
+            Console.WriteLine("París");
+            Console.WriteLine("Ya no vuelvo a aquí");
+
+            Console.WriteLine();
+
+            Console.WriteLine("No te necesito");
+            Console.WriteLine("Lo que necessito es");
+
+            Console.WriteLine();
+
+            Console.ForegroundColor=ConsoleColor.DarkMagenta;
+
+            Console.WriteLine("Tiempo pa' olvidar");
+            Console.WriteLine("Copas pa' beber");
+            Console.WriteLine("Y algo pa' este mal");
+            Console.WriteLine("Porque me voy a enloquecer");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Tiempo pa' olvidar");
+            Console.WriteLine("Copas pa' beber");
+            Console.WriteLine("Y algo pa' este mal");
+            Console.WriteLine("Porque me voy a enloquecer");
+            Console.WriteLine("(Poquer me voy a enloquecer)");
+
+            Console.WriteLine();
+
+            Console.ForegroundColor=ConsoleColor.Blue;
+
+            Console.WriteLine("Ya no sé qué hacer pa' olvidarte");
+            Console.WriteLine("Dímelo, Becky, Becky, Becky G");
+            Console.WriteLine("(Hey, Lexuz)");
+            Console.WriteLine("Abraham Mateo, eo, eo");
+            Console.WriteLine("(Ily Wonder)");
+            Console.WriteLine("Keityn");
+            Console.WriteLine("(Mosty)");
+            Console.WriteLine("Tiempo pa' olvidar");
+            Console.WriteLine("Desde Cádiz para el mundo");
+        }
+    }
+}
